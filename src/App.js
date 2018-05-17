f40c807a622cf72282b5793c1c8df944695ea8dd
m4rc0z <m.zirkenbach@gmx.de>
2018-05-17 19:50:39 +0200

    change something little to trigger build

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          My First React App now its hacking time !!
+          My First React App now its hacking time !!!
         </p>
       </div>
     );
